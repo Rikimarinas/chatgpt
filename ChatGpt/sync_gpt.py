d4590e09c4571c9a2acdd171683f1bee2d7a7f41
Rikimarinas <119539799+Rikimarinas@users.noreply.github.com>
2024-01-23 15:34:35 +0100

    Add files via upload

---
+++ ChatGpt/sync_gpt.py
@@ -0,0 +1,236 @@
+from playwright.sync_api import sync_playwright
+import tabulate
+import os
+from .Data import *
+
+
+def init():
+    global playwright, selected_browser, browser, f_time
+    # Inicializa el navegador Playwright
+    playwright = sync_playwright().start()
+    selected_browser = playwright.firefox
+    # Lanza una nueva instancia del navegador en modo headless (sin interfaz gráfica)
+    f_time = not os.path.isdir(os.path.join(os.getcwd(), "ChatGpt/cache_gpt"))
+    browser = selected_browser.launch_persistent_context(
+        os.path.join(os.getcwd(), "ChatGpt/cache_gpt"), headless=not f_time
+    )
+
+
+class ChatGpt:
+    def __init__(
+        self,
+        prompt="",
+        lnk="https://chat.openai.com/",
+        repeat_prompt=False,
+    ) -> None:
+        self.link = lnk
+        self.peticions = 0
+        self.prompt = prompt
+        self.repeat_prompt = repeat_prompt
+        self.running = False
+
+    def run(self):
+        # abre una nueva página
+        self.page = browser.new_page()
+        print("Accediendo a OpenAI..")
+        # Bloquea las rutas de las hojas de estilo y la API de moderación
+        if not f_time:
+            self.page.route("**/*.css", lambda route: route.abort())
+            self.page.route(
+                "https://chat.openai.com/backend-api/moderations",
+                lambda route: route.abort(),
+            )
+        # Navega a la página de OpenAI
+
+        if f_time:
+            try:
+                self.page.goto(self.link, wait_until="commit")
+                input("Please login on Chat-Gpt, this is only first time, press enter to continue..")
+
+
+            except Exception as e:
+                print(str(e))
+                exit(
+                    "Please Remove folder 'cache_gpt' from 'ChatGpt' folder to try login again"
+                )
+            print("Login successfully")
+            exit("Relaunch Program")
+        else:
+            self.page.goto(self.link, wait_until="domcontentloaded")
+        # Define los selectores de los boto nes de politicas
+        boton = PRIMERBOTON
+        boton2 = SEGUNDOBOTON
+
+        if not "login" in self.page.url:
+            print("Aceptando politicas..")
+        else:
+            try:
+                print("Please delete cache_gpt folder, session expired")
+                self.stop()
+                
+
+            except Exception as e:
+                print(str(e))
+                exit(
+                    "Please Remove folder 'cache_gpt' from 'ChatGpt' folder to try login again"
+                )
+            exit("Relaunch Program")
+
+        trying = 0
+        politics = False
+        while True:
+            try:
+                self.page.wait_for_selector(f"xpath={boton}", timeout=500)
+                politics = True
+                break
+            except:
+                if trying > 4:
+                    break
+                trying += 1
+                if trying % 2 == 0:
+                    boton = boton.replace("re", "rd")
+                    boton2 = boton2.replace("re", "rd")
+                else:
+                    boton = boton.replace("rd", "re")
+                    boton2 = boton2.replace("rd", "re")
+        if politics:
+            button = self.page.locator(f"xpath={boton}")
+            button.click()
+            button = self.page.locator(f"xpath={boton2}")
+            button.click()
+            button = self.page.locator(f"xpath={boton2}")
+            button.click()
+        else:
+            print("Politics Don't finded(Don't worry)")
+
+        self.running = True
+        if self.prompt != "":
+            print("Prompt Inicial..")
+            try:
+                self.page.wait_for_timeout(200)
+                self.peticion(self.prompt)
+            except Exception as e:
+                print(e)
+
+    def get_request(self):
+        t = (
+            self.page.locator(f"xpath={GPTREQUEST}")
+            .last.locator(f"xpath={CONTAINERTEXT}")
+            .locator("xpath=*")
+        )
+        text = ""
+        # LAS ANALIZO Y DEPENDIENDO DE LA RESPUESTA RESPONDE DE UNA U OTRA FORMA
+        for x in range(t.count()):
+            x = t.nth(x)
+            Tn = x.evaluate("(e) => e.tagName")
+
+            match Tn:
+                case "P":
+                    text += x.inner_text() + "\n\n"
+                case "OL":
+                    x = x.locator("li")
+                    for i in range(x.count()):
+                        li = x.nth(i)
+                        itext = li.inner_text(timeout=5000)
+                        text += f"\n{i+1}: {itext}\n"
+                    text += "\n\n"
+                case "UL":
+                    x = x.locator("li")
+                    for i in range(x.count()):
+                        li = x.nth(i)
+                        itext = li.inner_text(timeout=5000)
+                        text += f"\n-. {itext}\n"
+                    text += "\n\n"
+                case "TABLE":
+                    Headers = (
+                        x.locator("xpath=thead/tr").all_inner_texts()[0].split("\t")
+                    )
+                    filas = x.locator("xpath=tbody/tr").all_inner_texts()
+                    filas = list(map(lambda x: x.split("\t"), filas))
+
+                    tabla = (
+                        tabulate.tabulate(filas, Headers, "fancy_grid", maxcolwidths=40)
+                        + "\n\n"
+                    )
+                    text += tabla
+                case "PRE":
+                    # Título de la ventana
+                    try:
+                        T = x.locator(".bg-black.rounded-md.mb-4").locator("span").first
+                        Title = T.inner_text()
+                        text += f"\n{Title}\n"
+                    except:
+                        text += "\nCodigo\n"
+                    # Código
+                    Code = (
+                        x.locator(".bg-black.rounded-md.mb-4")
+                        .locator("xpath=/*")
+                        .all()[1]
+                        .inner_text()
+                    )
+                    text += f"\n{Code}\n"
+                case _:
+                    print(f"soy un {Tn}")
+        text = text.strip("\n\n")
+
+        if text == "":
+            self.peticion("regenerate")
+
+        return text
+
+    def peticion(self, Question):
+        if not self.running:
+            raise Exception("Chat Gpt not is running")
+
+        entrada = self.page.query_selector("#prompt-textarea")
+
+        if Question == "regenerate" and self.peticions > 0:
+            self.check.first.click()
+        else:
+            entrada.fill(
+                f"({self.prompt}){Question}" if self.repeat_prompt else Question
+            )
+            self.page.wait_for_timeout(100)
+            entrada.focus()
+            self.page.keyboard.press("Enter")
+            self.peticions += 1
+
+        self.check = self.page.locator(f"xpath={CHECKFULL}").locator("div")
+
+        while self.check.inner_text().lower() != "regenerate":
+            self.page.wait_for_timeout(100)
+            if self.check.count() > 1:
+                self.check.nth(1).click()
+
+        return self.get_request()
+
+    def stop(self):
+        if self.running:
+            eliminate = True
+            try:
+                btn = self.page.query_selector_all("button.p-1.hover\\:text-white")
+                if btn != []:
+                    btn[-1].click(timeout=1000)
+                else:
+                    pass
+                btn = self.page.query_selector_all("btn.relative.btn-danger")
+                if btn != []:
+                    btn[0].click(timeout=1000)
+                else:
+                    eliminate = False
+                if eliminate:
+                    print("\reliminando chat de openai..")
+                self.running = False
+                self.page.close()
+                browser.close()
+            except:
+                if not eliminate:
+                    print("Error en borrado de chat")
+
+    def __enter__(self):
+        init()
+        self.run()
+        return self
+
+    def __exit__(self,*args):
+        self.stop()
---
+++ README.md
@@ -0,0 +1,74 @@
+# Documentación del código
+ Este código es un script de Python que utiliza la biblioteca Playwright para interactuar con la página web de OpenAI. El script inicia un navegador, navega a la página de OpenAI, acepta las políticas y luego interactúa con la interfaz de usuario de la página para hacer peticiones a la IA de OpenAI.
+ 
+  Para usar el proyecto este debe estar en la misma carpeta que el archivo main, del cual se ejecutara el programa principal.
+
+ No olvidar usar el 
+ 
+ ```
+ playwright install firefox
+ ```
+ 
+En caso de que windows no te reconozca ese comando, debes asegurarte que Playwright este instalado, en caso que si lo este, usa
+
+```
+python -m playwright install firefox
+```
+
+ para que funcione correctamente, en el caso de querer usar otro navegador reemplazar el `selected_browser` en la función init de "sync_gpt.py" o "async_gpt.py"
+ ## Bibliotecas utilizadas
+ - **playwright**: Permite automatizar la interacción con los navegadores web.
+- **tabulate**: Ayuda a crear tablas bonitas y fáciles de leer.
+- **os**: Proporciona funciones para interactuar con el sistema operativo.
+- **Data**: Un archivo local del que se importan algunas constantes.
+ ## Funciones y Clases
+ ### init()
+ Esta función inicializa el navegador Playwright y lanza una nueva instancia del navegador en modo headless (sin interfaz gráfica).
+ ### ChatGpt
+ Esta es la clase principal que se utiliza para interactuar con la IA de OpenAI.
+ #### `__init__(self, prompt="", lnk="https://chat.openai.com/", repeat_prompt=False) `
+ Este es el constructor de la clase. Inicializa varias variables de instancia y registra la función ` stop ` para ser llamada cuando el programa termine.
+ 
+ ##### prompt
+ se refiere al mensaje inicial dado a la ia, muy util en el caso que se requiera dar algún tipo de personalidad
+
+ ##### lnk(Link) 
+ hace referencia a un chat pre-guardado, chatgpt permite reutilizar tus chats copiando el link al cual tenga al momento de cambiar de chat
+
+ ##### repeat_prompt
+  se refiere a que si quiere que el prompt inicial sea recordado a ChatGpt en  cada petición usando la sintaxis "(Prompt)(Petición)"
+ ##### ` run(self) `
+ Esta función inicia la interacción con la página de OpenAI. abre una nueva página, navega a la página de OpenAI, acepta las políticas y luego hace una petición inicial a la API de OpenAI.
+ #### ` get_request(self) `
+ Esta función obtiene la respuesta de la API de OpenAI. Analiza el contenido de la página para extraer la respuesta en texto.
+ #### ` peticion(self, Question) `
+ Esta función hace una petición a la pagina de ChatGpt. Escribe la pregunta en el área de texto de la página y luego presiona la tecla Enter.
+ #### ` stop(self) `
+ Esta función detiene la interacción con la página de OpenAI. Cierra la página y el navegador.
+ ## Uso
+ Para usar este script, simplemente importa la clase ` ChatGpt`, crea una instancia de la clase y luego llama a sus métodos. Por ejemplo:
+
+### SINCRÓNICO
+```python
+from ChatGpt.sync_gpt import ChatGpt
+
+with ChatGpt() as c:
+    response = c.peticion("¿Cómo estás?")
+    print(response)
+
+```
+### ASÍNCRONO
+```python
+from ChatGpt.async_gpt import ChatGpt
+import asyncio
+
+async def main():
+    async with ChatGpt() as c:
+        response = await c.peticion("¿Cómo estás?")
+        print(response)
+
+asyncio.run(main())
+
+```
+
+Este script es una forma eficiente de interactuar con la pagina de ChatGpt, ya que el navegador bloquea la carga de css, lo que acelera la carga en conexiones lentas, en solo un par de segundos ya se tiene una conexión establecida con ChatGpt(todo dependiendo de la conexión a internet que se posea) completamente gratis.
